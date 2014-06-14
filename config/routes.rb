ee2cb44fa1579da46added17d8ca158af7b4d974
DevPoint Labs <DPL1@devpointsimac3.domain.actdsltmp>
2014-06-14 13:39:00 -0600

    added routes and css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'static/home'
+
+  get 'static/about'
+  get 'static/blog'
+  root 'static#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
