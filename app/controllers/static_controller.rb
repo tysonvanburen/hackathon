2e2552423e11f49cc48260db6090fdce1003a7e1
justindruiz <justinruizwrestling@gmail.com>
2014-06-14 16:01:32 -0600

    made a bunch of pages

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,6 @@
 class StaticController < ApplicationController
   def home
-    
+    @posts = Post.all
   end
 
   def about
--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -38,6 +38,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_limit => [200, 200]
   end
 
+  version :mini do
+    process :resize_to_limit => [50, 50]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
